523c196c0f9369e5fcc4572b17cfef4d51b34191
mikito.maruyama <mikito.maruyama@sacn.co.jp>
2023-11-21 16:05:55 +0900

    11/20 Chapter5

---
+++ Chapter5/Example/Example/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Xml.Linq;
+
+//namespace Example
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            List<float> weights = new List<float>();
+
+//            weights.Add(41.2f);
+//            weights.Add(42.5f);
+//            weights.Add(44.9f);
+//            weights.Add(43.2f);
+//            weights.Add(43.2f);
+//            weights.Add(42.7f);
+//            weights.Add(41.7f);
+
+//            weights.Sort();
+
+//            for (int i = 0; i < 3; i++)
+//            {
+//                Console.WriteLine(weights[i]);
+//            }
+//        }
+//    }
+//}
