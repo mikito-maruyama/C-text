47d2ef87e143b61153e08fbb864732eba4350f25
mikito.maruyama <mikito.maruyama@sacn.co.jp>
2023-11-24 19:20:49 +0900

    11/24 Capter7

---
+++ Chapter7/WeatherChecker/WeatherChecker/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WeatherChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.areaBox = new System.Windows.Forms.ComboBox();
+            this.weatherIcon = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.weatherIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // areaBox
+            // 
+            this.areaBox.FormattingEnabled = true;
+            this.areaBox.Location = new System.Drawing.Point(167, 337);
+            this.areaBox.Name = "areaBox";
+            this.areaBox.Size = new System.Drawing.Size(121, 20);
+            this.areaBox.TabIndex = 0;
+            this.areaBox.SelectedIndexChanged += new System.EventHandler(this.CitySelected);
+            // 
+            // weatherIcon
+            // 
+            this.weatherIcon.Location = new System.Drawing.Point(402, 90);
+            this.weatherIcon.Name = "weatherIcon";
+            this.weatherIcon.Size = new System.Drawing.Size(309, 267);
+            this.weatherIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.weatherIcon.TabIndex = 1;
+            this.weatherIcon.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(165, 231);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "都道府県を選択";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.weatherIcon);
+            this.Controls.Add(this.areaBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.weatherIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox areaBox;
+        private System.Windows.Forms.PictureBox weatherIcon;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Chapter7/WeatherChecker/WeatherChecker/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+
+namespace WeatherChecker
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> cityNames;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.cityNames = new Dictionary<string, string>();
+
+            this.cityNames.Add("東京都", "3");
+            this.cityNames.Add("大阪府", "1");
+            this.cityNames.Add("愛知県", "2");
+            this.cityNames.Add("福岡県", "10");
+
+            foreach (KeyValuePair<string, string> data in this.cityNames)
+            {
+                areaBox.Items.Add(data.Key);
+            }
+        }
+
+        private void CitySelected(object sender, EventArgs e)
+        {
+            string cityCode = cityNames[areaBox.Text];
+            string url =
+                "https://and-idea.sbcr.jp/sp/90261/wheatherCheck.php?city=" +
+                cityCode;
+            HttpClient client = new HttpClient();
+            string result = client.GetStringAsync(url).Result;
+
+            JObject jobj = JObject.Parse(result);
+            string todayWeatherIcon = (string)((jobj["url"] as JValue).Value);
+            weatherIcon.ImageLocation = todayWeatherIcon;
+        }
+    }
+}
